bc343684643f030568269ecb72e6c342ecd2f13c
brian120488 <brian.yang@csedge.org>
2019-02-09 12:46:31 -0500

    Updated Website

---
+++ script.js
@@ -0,0 +1,4 @@
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "NEW HEADING";
+let about = document.getElementById("about");
+about.appendChild(newHeading);
---
+++ test.js
@@ -0,0 +1,8 @@
+let p = {
+    tagName: "P",
+    innerHTML: "<del>911</del>",
+    style: {
+        fontSize: "1.25em",
+        color: "blue"
+    }
+};
