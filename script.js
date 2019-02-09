715600dffab4ed2b83eb9e71715347cde4baf4e5
brian120488 <brian.yang@csedge.org>
2019-02-09 14:26:00 -0500

    Updated Javascript

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let newHeading = document.createElement("h1");
-newHeading.innerHTML = "NEW HEADING";
 let about = document.getElementById("about");
-about.appendChild(newHeading);
+
+about.addEventListener("click", whenClicked);
+
+let x = 0;
+
+function whenClicked() {
+	if(x == 0) {
+		let newHeading = document.createElement("h1");
+		newHeading.innerHTML = "NEW HEADING";
+		about.appendChild(newHeading);
+		newHeading.style.color = "blue";
+		newHeading.style.fontSize = "0.75em";
+		x++;
+	}
+}
